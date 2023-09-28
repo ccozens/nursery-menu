3d053973a416dcd65f45d9564cbbb14ee6d544b9
ccozens <ccozens@gmail.com>
2023-09-28 12:11:11 +0100

    remvoed page data store call from layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,17 +3,12 @@
 	import '../app.postcss';
 	import { isVegetarian } from '$lib/stores';
 	import { MainGrid } from '$lib/components';
-	import { page } from '$app/stores';
-
-	$: weekNumber = $page.data.weekNumber;
-
+	import { getCurrentWeekNumber } from '$lib/functions';
 	import { onMount } from 'svelte';
 	import { goto } from '$app/navigation';
-	import { getCurrentWeekNumber } from '$lib/functions';
 
 	const date = new Date();
 	const currentWeekNumber = getCurrentWeekNumber(date);
-
 
 	onMount(() => {
 		goto(`/${currentWeekNumber}`);
@@ -25,7 +20,7 @@
 <div class="wrapper">
 	<header>
 		<h1>Nursery menu</h1>
-		<div>Week: {weekNumber}</div>
+		<div>Week: {currentWeekNumber}</div>
 	</header>
 	<main>
 		<button on:click={() => ($isVegetarian = !$isVegetarian)}
