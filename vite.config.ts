f9c3c030f8123d17020efe3962b7376753738e30
ccozens <ccozens@gmail.com>
2023-10-04 14:09:26 +0100

    added this fix https://github.com/prisma/prisma/issues/12504#issuecomment-1541963336 for prisma type error and it builds successfully locally

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
+import { createRequire } from 'module';
+import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+
+const require = createRequire(import.meta.url);
+
+const prismaClient = require
+	.resolve('@prisma/client')
+	.replace(/@prisma(\/|\\)client(\/|\\)index\.js/, '.prisma/client/index-browser.js');
+
+const prismaIndexBrowser = path.normalize(path.relative(process.cwd(), prismaClient));
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	resolve: {
-		alias: {
-			'.prisma/client/index-browser': './node_modules/.prisma/client/index-browser.js'
-		}
+		alias: { '.prisma/client/index-browser': prismaIndexBrowser }
 	}
 });
