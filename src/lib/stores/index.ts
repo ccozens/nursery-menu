b537194eb156e7cce800ab229b9284e62fdbde93
ccozens <ccozens@gmail.com>
2023-10-06 15:29:52 +0100

    removed unused stores/days.ts, added derived store currentDay to stores/weekNumber.ts to return yesterday, today and tomorrow vars as days

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,4 +1,3 @@
-export * from './days';
 export * from './pages';
 export * from './isVegetarian';
 export * from './weekNumber';
--- src/lib/stores/weekNumber.ts
+++ src/lib/stores/weekNumber.ts
@@ -1,6 +1,15 @@
 import { readable, derived } from 'svelte/store';
 import { getCurrentWeekNumber } from '$lib/functions';
+import { days } from '$lib/data';
 
 export const date = readable(new Date());
 
-export const currentWeekNumber = derived(date, $date => getCurrentWeekNumber($date));
+export const currentWeekNumber = derived(date, ($date) => getCurrentWeekNumber($date));
+
+export const currentDay = derived(date, ($date) => {
+	const todayNumber = $date.getDay();
+	const today = days[todayNumber];
+	const yesterday = days[todayNumber - 1];
+	const tomorrow = days[todayNumber + 1];
+	return { today, yesterday, tomorrow };
+});
